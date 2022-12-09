928c26928877ec3e4082546eaa6e56f2fa360e40
Serkan <43848989+serkancakmakk@users.noreply.github.com>
2022-12-09 15:42:28 +0300

    python-integers-come-in-all
    python-integers-come-in-all

--- python-integers-come-in-all.py
+++ python-integers-come-in-all.py
@@ -1,4 +1,5 @@
-# Integers in Python can be as big as the bytes in your machine's memory. There is no limit in size as there is:  (c++ int) or  (C++ long long int).
+# Integers in Python can be as big as the bytes in your machine's memory. 
+#There is no limit in size as there is:  (c++ int) or  (C++ long long int).
 #
 # As we know, the result of
 # grows really fast with increasing .
